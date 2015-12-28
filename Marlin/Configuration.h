34de0a590d92e1f0a191c95e39bd1fc04670dfaf
branodn miller <github@spam.firepunk.com>
2015-12-27 19:26:00 -0500

    changed x max y max

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -334,9 +334,9 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 200
+#define X_MAX_POS 215
 #define X_MIN_POS 0
-#define Y_MAX_POS 200
+#define Y_MAX_POS 240
 #define Y_MIN_POS 0
 #define Z_MAX_POS 140
 #define Z_MIN_POS 0
